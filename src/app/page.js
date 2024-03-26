df238c4f18f562d833c38af70cb733e8c4623776
DChristensen03 <dylanchristensen2000@gmail.com>
2024-03-26 11:31:21 -0500

    Add images to homescreen

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,60 @@
+"use client";
+import { Badge } from "flowbite-react";
+import { useState } from "react";
+import Link from "next/link";
+
+/* eslint-disable @next/next/no-img-element */
 export default function Home() {
+	const pages = ["Vehicles", "Drivers", "Maintenance", "Scheduling"];
+	const [hoverState, setHoverState] = useState({
+		Vehicles: false,
+		Drivers: false,
+		Maintenance: false,
+		Scheduling: false,
+	});
+
+	const alterHoverState = (page, state) => {
+		const newState = { ...hoverState };
+		newState[page] = state;
+		setHoverState(newState);
+	};
+
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-3">
-			<h1 className="text-7xl font-bold italic">
+		<main className="grid auto-rows-auto mx-5 space-y-3">
+			<h1 className="text-7xl text-center w-full font-bold italic">
 				<span>Fleet</span>
 				<span className="underline text-primary-600">Ease</span>
 			</h1>
+			<div class="grid grid-cols-4 gap-4 justify-start">
+				{pages.map((page) => (
+					<Link key={page} href={`/management/${page.toLowerCase()}`}>
+						<div
+							className={`rounded relative border-2 p-1 ${
+								hoverState[page] ? "m-2" : "m-4"
+							}`}
+							onMouseEnter={() => alterHoverState(page, true)}
+							onMouseLeave={() => alterHoverState(page, false)}
+						>
+							<div className="bg-white rounded">
+								<img
+									src={`/${page}.jpg`}
+									alt={page}
+									width="100%"
+									height="100%"
+									className="rounded opacity-90 dark:opacity-80"
+								/>
+							</div>
+							<Badge
+								className="absolute bottom-3 right-1 w-3/4 min-h-12 h-1/5 max-h-32 place-content-center rounded-none"
+								size="lg"
+								color={"dark"}
+							>
+								{page.toUpperCase()}
+							</Badge>
+						</div>
+					</Link>
+				))}
+			</div>
 		</main>
 	);
 }
